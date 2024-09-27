f416f2294c18c50401b7b356c59d4f6c53d8fcf1
Shai Eliav <yuvaleliav@Shais-MacBook-Pro.local>
2024-09-27 15:00:01 +0300

    basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import GridTV from "./components/GridTV";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const channels = {
+        israeli: {
+            news: [
+                {
+                    name: "Kan-11",
+                    channel: 11,
+                    url: "https://kan11w.media.kan.org.il/hls/live/2105694/2105694/source1_4k/chunklist.m3u8",
+                },
+                {
+                    name: "Keshet-12",
+                    channel: 12,
+                    url: "https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12dvr/profile/0/hdntl=exp=1726926801~acl=%2f*~data=hdntl~hmac=99f779508cc7547d56cf41ee596705bfff3964fcbbf39a97c553eb62bf5c2d67/profileManifest.m3u8?_uid=6e55305f-72f1-40ec-8d45-32b40b8caec2&rK=a1&_did=Wd720f40fa73264f1c45804dc924082f89b4",
+                },
+                {
+                    name: "Reshet-13",
+                    channel: 13,
+                    url: "https://reshet.g-mana.live/media/cdefce3a-14ec-46cc-a147-1275c4a8b9ed/profile/1/profileManifest.m3u8?_uid=b1915715-db1f-4c13-91cd-6e97f687e445&rK=a6&_did=f8603b08-db94-44ce-9a30-548209ef5609",
+                },
+                {
+                    name: "Channel-14",
+                    channel: 14,
+                    url: "https://ch14-channel14-content.akamaized.net/hls/live/2104807/CH14_CHANNEL14/2/streamPlaylist.m3u8",
+                },
+            ],
+        },
+    };
+
+    return (
+        <div className="App" style={{height: "100%", flex: 1}}>
+            <Router>
+                <Routes>
+                    <Route
+                        path="/tv-dashboard"
+                        exact
+                        Component={() => {
+                            return (
+                                <div>
+                                    <h1>Home</h1>
+                                </div>
+                            );
+                        }}
+                    ></Route>
+                    <Route
+                        path="/tv-dashboard/israeli/:style"
+                        element={<GridTV channelsArray={channels.israeli.news}/>}
+                    />
+                    <Route
+                        path="/tv-dashboard/international"
+                        Component={() => {
+                            return (
+                                <div>
+                                    <h1>International</h1>
+                                </div>
+                            );
+                        }}
+                    ></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/GridTV.js
@@ -0,0 +1,137 @@
+import {useEffect, useRef} from "react";
+import Video from "./Video";
+import {useParams} from "react-router-dom";
+
+const GridTV = ({channelsArray}) => {
+    const playerRef = useRef({});
+    const videoRef = useRef({});
+    const currentChannel = useRef(0);
+    const {style} = useParams();
+
+    useEffect(() => {
+        const listener = (e) => {
+            const handleMuted = (player) => {
+                Object.entries(playerRef.current).forEach(([key, value]) => {
+                    if (key !== player) {
+                        value.muted(true);
+                    }
+                });
+                if (playerRef.current[player]) {
+                    playerRef.current[player].muted(false);
+                }
+                if(videoRef.current[player]){
+                    videoRef.current[player].current.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+                }
+            };
+
+            if (
+                parseInt(e.key) >= 0 &&
+                parseInt(e.key) <= Object.keys(playerRef.current).length
+            ) {
+                currentChannel.current = +e.key;
+                handleMuted(e.key);
+            }
+            if (e.key === "ArrowRight") {
+                if (
+                    currentChannel.current <
+                    Object.keys(playerRef.current).length
+                ) {
+                    currentChannel.current++;
+                    handleMuted(currentChannel.current);
+                } else if (
+                    currentChannel.current ===
+                    Object.keys(playerRef.current).length
+                ) {
+                    currentChannel.current = 1;
+                    handleMuted(currentChannel.current);
+                }
+            }
+            if (e.key === "ArrowLeft") {
+                if (currentChannel.current > 1) {
+                    currentChannel.current--;
+                    handleMuted(currentChannel.current);
+                } else if (currentChannel.current === 1) {
+                    currentChannel.current = Object.keys(
+                        playerRef.current
+                    ).length;
+                    handleMuted(currentChannel.current);
+                }
+            }
+        };
+        window.addEventListener("keydown", listener);
+
+        return () => {
+            window.removeEventListener("keydown", listener);
+        };
+    }, [playerRef.current]);
+
+    const defaultOptions = (src) => {
+        return {
+            muted: true,
+            autoplay: true,
+            controls: true,
+            responsive: true,
+            fluid: true,
+            liveui: true,
+            sources: [
+                {
+                    src: src,
+                    type: "application/x-mpegURL",
+                },
+            ],
+        };
+    };
+
+    const styles = {
+        grid: {
+            screen: {flex: 1, height: "100%"},
+            width: 200,
+            container: {
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                marginRight: 10,
+                marginLeft: 10,
+                gap: 20,
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                height: "20%",
+            },
+        },
+        slider: {
+            screen: {display: "flex", flex: 1, flexDirection: 'row', height: "100%"},
+            width: window.innerWidth * 0.9,
+            container: {
+                display: "flex",
+                flexDirection: "row",
+                marginRight: 10,
+                marginLeft: 10,
+                gap: 20,
+                height: "100%",
+            },
+        },
+    };
+
+    return (
+        <div style={styles[style].screen}>
+            <h1>Israeli News Dashboard</h1>
+            <div style={styles[style].container}>
+                {channelsArray?.map((channel, index) => {
+                    return (
+                        <Video
+                            options={defaultOptions(channel.url)}
+                            key={channel.name}
+                            getPlayer={(player, video) => {
+                                playerRef.current[index + 1] = player;
+                                videoRef.current[index + 1] = video;
+                            }}
+                            width={styles[style].width}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default GridTV;
